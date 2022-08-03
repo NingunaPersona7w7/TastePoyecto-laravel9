cfbc01f65623614d84867ac9450db0426e19cb74
JuanESena <juanbarrios072@gmail.com>
2022-08-03 17:27:52 -0500

    primeras tablas

--- database/migrations/2022_08_03_211459_create_posts_table.php
+++ database/migrations/2022_08_03_211459_create_posts_table.php
@@ -14,10 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-
-            $table->string('title');
-
+            $table->id();
             $table->timestamps();
         });
     }
