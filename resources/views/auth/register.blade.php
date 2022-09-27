99d2a155a25dd96db3d3eca2cecb493a4112df89
Jeisson Martinez <jeissonandrey.martinezh@gmail.com>
2022-09-27 03:32:51 -0500

    cosas y cambios, recuerda probar todo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -66,28 +66,7 @@
                     </label>
                 </div>
                 <div class="input-group">
-<<<<<<< HEAD
-                    {!! Form::open(['route' => ['users.RolStore'], 'method' => 'POST']) !!}
-                        {!! Form::label('rol', 'Rol') !!}
-                        {!! Form::select('role', $roles, [], ['class' => 'form-control']) !!}
-                    {!! Form::close() !!}
 
-||||||| 79ab20b
-                    <form action=""></form>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Roles</option>
-                        <option value="1">Comprador </option>
-                        <option value="2">Vendedor</option>
-                      </select>
-                    </form>
-                    <div class="form-group1"{{$user->assignRole($request->input('roles'));}}>
-                        {!! Form::label('rol', 'Rol') !!}
-                        {!! Form::select('role', $roles, [], ['class' => 'form-control']) !!}
-                    </div>
-=======
-
-
->>>>>>> 663f05a50a8604463cff12813f2e2b3a80c3503a
                 </div>
                 <a href="{{ url('/login') }}">¿Tienes una cuenta?</a>
                 <input type="submit" value="Registrar" class="btn-login" />
